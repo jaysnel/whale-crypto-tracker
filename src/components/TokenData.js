133b390dc8d2d7ba5b41a9c3e0bc0b003f5a8c2e
jaysnel <jaylan210@gmail.com>
2023-01-28 23:02:32 -0500

    currently logging out all whales above a number that i clearly dont understand...but its working lol. i need more research

--- src/components/TokenData.js
+++ src/components/TokenData.js
@@ -8,15 +8,11 @@ export default function TokenData() {
   const tokenList = require('../token-address/tokens.json')
   const generalABI = require('../abi/GENERAL_ERC20_CONTRACT_ABI.json')
   const getToken = (coin) => {
-    try {
-      const currentToken = tokenList.filter(tkn => tkn.name === coin)
+    const currentToken = tokenList.filter(tkn => tkn.name === coin)
       return {
           name: currentToken[0].name, 
           address: currentToken[0].address
-      };
-    } catch (err) {
-      console.log(err)
-    }
+      }
   }
   const getTokenData = async() => {
     try {
@@ -26,8 +22,15 @@ export default function TokenData() {
         const provider = new ethers.providers.JsonRpcProvider(ethRpcURL)
         const contract = new Contract(tokenContractAddresss.address, generalABI, provider)
         const name = await contract.name()
+        const Transfer_Threshold = 1000000000
+        
         setName(name)
-        console.log('Data: ', name)
+        
+        contract.on('Transfer', (from, to, amount, data) => {
+          if(amount.toNumber() >= Transfer_Threshold) {
+            console.log(`New Whale Transfer for: ${name} - https://etherscan.io/tx/${data.transactionHash}`)
+          }
+        })
       }
     } catch (err) {
       console.log(err)
